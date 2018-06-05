16c4be7841def440a25efd111ed1f9cc893813f1
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-06-04 20:30:03 -0400

    Pickle Rick

--- wslstartmenu.ps1
+++ wslstartmenu.ps1
@@ -11,49 +11,7 @@
   Author:   Hayden Barnes (github.com/sirredbeard)
 #>
 
-# Set up logging
-
-Import-Module PSLogging
-$sLogPath = 'C:\Windows\Temp'
-$sLogName = 'wslstartmenu.log'
-$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
-
-# Functions
-
-<
-
-Function DoSomething {
-  Param ()
-
-  Begin {
-    Write-LogInfo -LogPath $sLogFile -Message '<description of what is going on>...'
-  }
-
-  Process {
-    Try {
-      <code goes here>
-    }
-
-    Catch {
-      Write-LogError -LogPath $sLogFile -Message $_.Exception -ExitGracefully
-      Break
-    }
-  }
-
-  End {
-    If ($?) {
-      Write-LogInfo -LogPath $sLogFile -Message 'Completed Successfully.'
-      Write-LogInfo -LogPath $sLogFile -Message ' '
-    }
-  }
-}
-
->
-
 # Execution
-
-Start-Log -LogPath $sLogPath -LogName $sLogName
-
 
 # Collect .desktop files from WSL rootfs: /etc/share/applications/%.desktop, /users/%/.local/share/applications/%.desktop
 # Dump them all into a single text file
@@ -61,7 +19,3 @@ Start-Log -LogPath $sLogPath -LogName $sLogName
 # Go back in for the corresponding icons
 # Erase anything in CSIDL_PROGRAMS\WSL Desktop Apps\
 # Create shortcuts in CSIDL_PROGRAMS\WSL Destop Apps\
-
-DoSomething
-
-Stop-Log -LogPath $sLogFile
